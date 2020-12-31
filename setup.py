3788bf2c5989d726933f01aa69febf26d3f8158a
Abhishek Thakur <abhishek4@gmail.com>
2020-12-31 18:30:41 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 if __name__ == "__main__":
     setup(
         name="tez",
-        version="0.0.8",
+        version="0.0.9",
         description="tez - a simple pytorch trainer...",
         long_description=long_description,
         long_description_content_type="text/markdown",
