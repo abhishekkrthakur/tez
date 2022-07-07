5459d681979b0bd5607bb6c04694ae59fc06fa89
Abhishek Thakur <abhishek4@gmail.com>
2022-07-07 16:40:54 +0200

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ with open("README.md") as f:
     long_description = f.read()
 
 
+INSTALL_REQUIRES = [
+    "loguru==0.6.0",
+    "accelerate==0.10.0",
+    "tqdm==4.64.0",
+]
+
+
 if __name__ == "__main__":
     setup(
         name="tez",
@@ -12,11 +19,11 @@ if __name__ == "__main__":
         long_description=long_description,
         long_description_content_type="text/markdown",
         author="Abhishek Thakur",
-        author_email="abhishek4@gmail.com",
         url="https://github.com/abhishekkrthakur/tez",
         license="Apache License",
         packages=find_packages(),
         include_package_data=True,
         platforms=["linux", "unix"],
-        python_requires=">=3.6",
+        python_requires=">=3.7",
+        install_requires=INSTALL_REQUIRES,
     )
